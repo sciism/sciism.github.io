31e3d819598f6dc7cc0b1f381729ed3c1f2dbbf4
aycippo <acippoletti@sciism.com>
2023-05-12 09:51:00 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sciism.github.io
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
